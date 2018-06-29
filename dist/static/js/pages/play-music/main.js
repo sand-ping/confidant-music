a3a6f5882dbf1ea496a290351d223287c8bf7445
jiangzeping1 <1092116709@qq.com>
2018-06-29 17:44:08 +0800

    增加歌词滚动

--- dist/static/js/pages/play-music/main.js
+++ dist/static/js/pages/play-music/main.js
@@ -68,18 +68,27 @@ app.$mount();
       lyric: "[00:00] 漂洋过海来看你" + ".[00:02] 演唱：刘明湘.[00:04] 词曲：李宗盛.[00:08] 歌词编辑：丁仔.[00:15] 中文歌词库 www.dingzai.com.[00:21] 为你我用了半年的积蓄.[00:24] 飘洋过海的来看你.[00:29] 为了这次相聚.[00:31] 我连见面时的呼吸 都曾反复练习.[00:36] .[00:38] 言语从来没能将我的情意.[00:42] 表达千万分之一.[00:47] 为了这个遗憾 我在夜里想了又想.[00:51] 不肯睡去.[00:54] .[00:55] 记忆它总是慢慢的累积.[00:59] 在我心中无法抹去.[01:04] 为了你的承诺.[01:05] 我在最绝望的时候 都忍着不哭泣.[01:13] .[01:14] 陌生的城市啊 熟悉的角落里.[01:23] 也曾彼此安慰 也曾相拥叹息.[01:26] 不管将会面对什么样的结局.[01:30] .[01:31] 在漫天风沙里望着你远去.[01:35] 我竟悲伤的不能自已.[01:39] 多盼能送君千里 直到山穷水尽.[01:44] 一生和你相依.[01:49] .[02:07] 陌生的城市啊 熟悉的角落里.[02:15] 也曾彼此安慰 也曾相拥叹息.[02:19] 不管将会面对什么样的结局.[02:23] .[02:23] 在漫天风沙里望着你远去.[02:27] 我竟悲伤的不能自已.[02:32] 多盼能送君千里 直到山穷水尽.[02:36] 一生和你相依.[02:42] .[02:43] 多盼能送君千里 直到山穷水尽.[02:50] 一生和你相依",
       lytime: [],
       lytext: [],
-      lyindex: 0,
-      lyheight: 180,
-      playClass: 'icon-bofang'
+      lycount: 0, //总共几行歌词
+      lyindex: 0, //当前歌词下标
+      lytop: 360, //距离顶部多高
+      lyheight: 40, //歌词行高
+      endtime: '00:00',
+      playClass: 'icon-bofang',
+      audioWatch: ''
     };
+  },
+  created: function created() {
+    this.lyricReady();
   },
 
   methods: {
+    //提取歌词和歌词对应的时间
     lyricReady: function lyricReady() {
       var root = this;
       var lyric = root.lyric;
-      console.log(lyric);
       var lyArr = lyric.split('.');
+      var endalltime = lyArr[lyArr.length - 1].split(' ')[0];
+      root.endtime = endalltime.substring(1, endalltime.length - 1);
       var lytime = [];
       var lytext = [];
       for (var i = 0; i < lyArr.length; i++) {
@@ -96,6 +105,28 @@ app.$mount();
       }
       this.lytime = lytime;
       this.lytext = lytext;
+      this.audioWatch = setInterval(function () {
+        wx.getBackgroundAudioPlayerState({
+          success: function success(res) {
+            var status = res.status;
+            if (status == 1) {
+              var currentPosition = res.currentPosition;
+              var duration = res.duration;
+              var _lytime = root.lytime;
+              var index = 0;
+              for (var _i = 0; _i < _lytime.length; _i++) {
+                if (currentPosition >= _lytime[_i]) {
+                  index = _i;
+                } else {
+                  break;
+                }
+              }
+              root.lyindex = index;
+              root.lytop = 360 - index * 40;
+            }
+          }
+        });
+      }, 2000);
     },
     play: function play() {
       if (this.playClass == 'icon-bofang') {
@@ -116,16 +147,7 @@ app.$mount();
         this.playClass = 'icon-bofang';
         wx.pauseBackgroundAudio();
       }
-      console.log('play');
-      //        this.audioCtx.play();
-
     }
-  },
-  created: function created() {
-    console.log('playmusic');
-    this.lyricReady();
-    //      this.audioCtx = wx.createAudioContext('myAudio')
-    //      this.audioCtx.play()
   }
 });
 
@@ -149,7 +171,7 @@ var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._sel
     staticClass: "body"
   }, [_c('div', {
     staticClass: "lyric",
-    style: ('top:' + _vm.lyheight + 'px')
+    style: ('top:' + _vm.lytop + 'rpx')
   }, _vm._l((_vm.lytext), function(item, index) {
     return _c('div', {
       staticClass: "lyric-item",
@@ -165,7 +187,7 @@ var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._sel
     staticClass: "progress-line"
   })]), _vm._v(" "), _c('div', {
     staticClass: "endtime"
-  }, [_vm._v("02:20")])]), _vm._v(" "), _c('div', {
+  }, [_vm._v(_vm._s(_vm.endtime))])]), _vm._v(" "), _c('div', {
     staticClass: "operate"
   }, [_c('div', {
     staticClass: "prev iconfont icon-xiayishou-copy"
--- src/pages/play-music/index.vue
+++ src/pages/play-music/index.vue
@@ -7,7 +7,7 @@
       </div>
     </header>
     <div class="body">
-      <div class="lyric" :style="'top:'+lyheight+'px'">
+      <div class="lyric" :style="'top:'+lytop+'rpx'">
         <div class="lyric-item" v-for="(item,index) in lytext" :style="index==lyindex?'color:red':''">
           {{item}}
         </div>
@@ -19,7 +19,7 @@
         <div class="progress">
           <div class="progress-line"></div>
         </div>
-        <div class="endtime">02:20</div>
+        <div class="endtime">{{endtime}}</div>
       </div>
       <div class="operate">
         <div class="prev iconfont icon-xiayishou-copy"></div>
@@ -41,17 +41,26 @@
         ".[00:02] 演唱：刘明湘.[00:04] 词曲：李宗盛.[00:08] 歌词编辑：丁仔.[00:15] 中文歌词库 www.dingzai.com.[00:21] 为你我用了半年的积蓄.[00:24] 飘洋过海的来看你.[00:29] 为了这次相聚.[00:31] 我连见面时的呼吸 都曾反复练习.[00:36] .[00:38] 言语从来没能将我的情意.[00:42] 表达千万分之一.[00:47] 为了这个遗憾 我在夜里想了又想.[00:51] 不肯睡去.[00:54] .[00:55] 记忆它总是慢慢的累积.[00:59] 在我心中无法抹去.[01:04] 为了你的承诺.[01:05] 我在最绝望的时候 都忍着不哭泣.[01:13] .[01:14] 陌生的城市啊 熟悉的角落里.[01:23] 也曾彼此安慰 也曾相拥叹息.[01:26] 不管将会面对什么样的结局.[01:30] .[01:31] 在漫天风沙里望着你远去.[01:35] 我竟悲伤的不能自已.[01:39] 多盼能送君千里 直到山穷水尽.[01:44] 一生和你相依.[01:49] .[02:07] 陌生的城市啊 熟悉的角落里.[02:15] 也曾彼此安慰 也曾相拥叹息.[02:19] 不管将会面对什么样的结局.[02:23] .[02:23] 在漫天风沙里望着你远去.[02:27] 我竟悲伤的不能自已.[02:32] 多盼能送君千里 直到山穷水尽.[02:36] 一生和你相依.[02:42] .[02:43] 多盼能送君千里 直到山穷水尽.[02:50] 一生和你相依",
         lytime:[],
         lytext:[],
-        lyindex:0,
-        lyheight:180,
+        lycount:0, //总共几行歌词
+        lyindex:0, //当前歌词下标
+        lytop:360, //距离顶部多高
+        lyheight:40,//歌词行高
+        endtime:'00:00',
         playClass:'icon-bofang',
+        audioWatch:'',
       }
     },
+    created () {
+      this.lyricReady();
+    },
     methods:{
+      //提取歌词和歌词对应的时间
       lyricReady:function () {
         var root=this;
         var lyric=root.lyric;
-        console.log(lyric)
         var lyArr=lyric.split('.');
+        var endalltime=lyArr[lyArr.length-1].split(' ')[0];
+        root.endtime=endalltime.substring(1,endalltime.length-1);
         var lytime=[];
         var lytext=[];
         for(let i=0;i<lyArr.length;i++){
@@ -68,6 +77,28 @@
         }
         this.lytime=lytime;
         this.lytext=lytext;
+        this.audioWatch=setInterval(function () {
+          wx.getBackgroundAudioPlayerState({
+            success: function(res) {
+              let status = res.status
+              if(status==1){
+                var currentPosition = res.currentPosition;
+                var duration = res.duration;
+                let lytime=root.lytime;
+                let index=0;
+                for(let i=0;i<lytime.length;i++){
+                  if(currentPosition>=lytime[i]){
+                    index=i;
+                  }else{
+                    break;
+                  }
+                }
+                root.lyindex=index;
+                root.lytop=360-index*40;
+              }
+            }
+          })
+        },2000)
       },
       play:function () {
         if(this.playClass=='icon-bofang'){
@@ -88,19 +119,8 @@
           this.playClass='icon-bofang';
           wx.pauseBackgroundAudio()
         }
-        console.log('play');
-//        this.audioCtx.play();
-
-
       }
     },
-    created () {
-      console.log('playmusic')
-      this.lyricReady();
-//      this.audioCtx = wx.createAudioContext('myAudio')
-//      this.audioCtx.play()
-    }
-
   }
 </script>
 
